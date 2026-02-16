6d3a8f816be9c8230145e76c47a415c43f2983f0
peshopbs2 <peshopbs2@gmail.com>
2026-02-16 09:13:38 +0200

    Add ASCII to text conversion with hex output

---
+++ ascii_to_text.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_hex(const char *text) {
+    printf("Symbol | Decimal | Hex\n");
+    printf("-------|---------|-----\n");
+    
+    for (int i = 0; i < strlen(text); i++) {
+        printf("%6c |%8d | %02x\n", text[i], text[i], text[i]);
+    }
+}
+
+int main() {
+    char message[] = "Cyber";
+    print_hex(message);
+    return EXIT_SUCCESS;
+}
